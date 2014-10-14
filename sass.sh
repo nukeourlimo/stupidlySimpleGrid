7b7d780db0113fec1b93e998a3bbdb048d809533
nukeourlimo <nukeourlimo@gmail.com>
2014-10-14 17:00:07 +0100

    simple grid assets

---
+++ sass.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Watch your SASS folder, and automatically compile to production ready compressed styles. This will remove all comments from your SCSS too.
+sass --watch scss:css --style compressed
